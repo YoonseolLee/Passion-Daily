36770c6ec0c9b49d2dfb562374553648b4488ed7
YoonseolLee <thisyoon97@gmail.com>
2024-11-02 19:53:03 +0900

    feat: MainActivity 추가

---
+++ app/src/main/java/com/example/passionDaily/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.passionDaily
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.example.passionDaily.navigation.SetupNavigation
+import com.example.passionDaily.ui.theme.Passion_DailyTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            Passion_DailyTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    val navController = rememberNavController()
+                    SetupNavigation(navController = navController)
+                }
+            }
+        }
+    }
+}
