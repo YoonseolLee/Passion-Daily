9e0c43fd122041492672ab45378532feebf4b5fc
YoonseolLee <thisyoon97@gmail.com>
2024-12-19 18:07:05 +0900

    refactor: Quote.kt 필드 수정 - 파이어베이스와 필드가 동일하도록 수정하였음

--- app/src/main/java/com/example/passionDaily/data/remote/model/Quote.kt
+++ app/src/main/java/com/example/passionDaily/data/remote/model/Quote.kt
@@ -4,26 +4,14 @@ import com.google.firebase.Timestamp
 import com.google.firebase.firestore.DocumentReference
 
 data class Quote(
-    val id: String = "",
-    val text: String = "",
-    val authorRef: DocumentReference? = null,
-    val imageUrl: String = "",
-    val categoryRef: DocumentReference? = null,
-    val views: Int = 0,
-    val shares: Int = 0,
-    val isDeleted: Boolean = false,
-    val createdDate: Timestamp = Timestamp.now(),
-    val modifiedDate: Timestamp = Timestamp.now(),
-    val category: CategoryInfo = CategoryInfo(),
-    val author: AuthorInfo = AuthorInfo()
-) {
-    data class CategoryInfo(
-        val id: String = "",
-        val name: String = ""
-    )
-
-    data class AuthorInfo(
-        val id: String = "",
-        val name: String = ""
-    )
-}
+    val id: String,
+    val category: String,
+    val text: String,
+    val person: String,
+    val imageUrl: String,
+    val createdAt: Long,
+    val modifiedAt: Long,
+    val isDeleted: Boolean,
+    val viewCount: Int,
+    val shareCount: Int
+)
