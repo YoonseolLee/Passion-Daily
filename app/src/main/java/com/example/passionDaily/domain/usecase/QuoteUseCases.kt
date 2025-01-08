a58424df4c233d93ea4eeaa5d9a9f31592a026bc
YoonseolLee <thisyoon97@gmail.com>
2025-01-08 10:54:05 +0900

    refactor: shareText 리팩터링 - viewmodel에서 usecases로 이동

---
+++ app/src/main/java/com/example/passionDaily/domain/usecase/QuoteUseCases.kt
@@ -0,0 +1,23 @@
+package com.example.passionDaily.domain.usecase
+
+import android.content.Context
+import android.content.Intent
+import javax.inject.Inject
+
+class QuoteUseCases @Inject constructor(
+) {
+
+    // TODO: 이미지 공유로 변경
+    fun shareText(context:Context, text: String) {
+        try {
+            val intent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, text)
+            }
+            val chooser = Intent.createChooser(intent, "공유하기")
+            context.startActivity(chooser)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
