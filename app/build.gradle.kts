afe5d0a6e6fade27a91d5f94a105a6750fc00324
YoonseolLee <thisyoon97@gmail.com>
2025-01-19 23:31:24 +0900

    build: firebase-messaging-ktx 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -112,4 +112,8 @@ dependencies {
 
     // coil
     implementation("io.coil-kt.coil3:coil-compose:3.0.4")
-    implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.4")}
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.4")
+
+    // FCM
+    implementation("com.google.firebase:firebase-messaging-ktx")
+}
