5d98fa2679da9974f2fc8ae5130f3a5a5d06d3bd
YoonseolLee <thisyoon97@gmail.com>
2025-02-09 22:00:17 +0900

    test: UrlManager 테스트 코드 작성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -183,4 +183,9 @@ dependencies {
 
     // json - 테스트환경
     testImplementation("org.json:json:20240303")
+
+    //
+    testImplementation ("org.robolectric:robolectric:4.11.1")
+    testImplementation("androidx.test:core:1.6.1")
+    testImplementation("androidx.test:core-ktx:1.6.0")
 }
